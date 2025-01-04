cb354741c9f8581fc34305dd554947121ffbfd05
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-01-04 16:35:57 +0100

    Implement product management templates and forms; add CSRF protection and validation

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,10 +18,82 @@ class ProductController extends AbstractController
     {
         $products = $repository->findAll();
 
-        dump($products);
-
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $products,
+        ]);
+    }
+
+    #[Route('/products/{id<\d+>}', name: 'product_show')]
+    public function show($id, ProductRepository $repository): Response
+    {
+        $product = $repository->findOneBy(['id' => $id]);
+
+        if (!$product) {
+            throw $this->createNotFoundException('Product not found');
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+    #[Route('/products/new', name: 'product_new')]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $product = new Product();
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($product);
+            $manager->flush();
+
+            $this->addFlash('notice', 'Product created successfully!');
+
+            return $this->redirectToRoute('product_show', ['id' => $product->getId()]);
+        }
+
+        return $this->render('product/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/products/{id<\d+>}/edit', name: 'product_edit')]
+    public function edit(Product $product, Request $request, EntityManagerInterface $manager): Response
+    {
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->flush();
+
+            $this->addFlash('notice', 'Product updated successfully!');
+
+            return $this->redirectToRoute('product_show', ['id' => $product->getId()]);
+        }
+
+        return $this->render('product/edit.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/products/{id<\d+>}/delete', name: 'product_delete')]
+    public function delete(Request $request, Product $product, EntityManagerInterface $manager): Response
+    {
+        if ($request->isMethod('POST')) {
+            $manager->remove($product);
+            $manager->flush();
+
+            $this->addFlash('notice', 'Product deleted successfully!');
+
+            return $this->redirectToRoute('product_index');
+        }
+
+        return $this->render('product/delete.html.twig', [
+            'id' => $product->getId(),
         ]);
     }
 }
